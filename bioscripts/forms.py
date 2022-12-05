8750378e9b30c42392c246b72e7ead79f2f0f3f4
bar <bar@E15G3.home>
2022-12-05 19:50:05 +0300

    Added gene symbol checker

--- bioscripts/forms.py
+++ bioscripts/forms.py
@@ -1,4 +1,23 @@
 from django import forms
+from django.forms import widgets
+from django.forms.widgets import Textarea
 
 class Var2TexShadeForm(forms.Form):
     hgvsp = forms.CharField(label='HGVSp', max_length=100)
+
+
+class GeneSymbolCheckerForm(forms.Form):
+    assemblies = [
+        ('GRCh37', 'GRCh37'),
+        ('GRCh38', 'GRCh38'),
+        ('T2T', 'T2T'),
+    ]
+
+    sources = [
+        ('Ensembl', 'Ensembl'),
+        ('RefSeq', 'RefSeq'),
+    ]
+
+    source = forms.ChoiceField(label="Annotation Source", choices=sources)
+    assembly = forms.ChoiceField(label="Genome Assembly", choices=assemblies)
+    symbols = forms.CharField(label='Gene Symbols', widget=Textarea())
--- bioscripts/views.py
+++ bioscripts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import FileResponse
+from django.http import FileResponse, StreamingHttpResponse
 from django.conf import settings
-from pathlib import Path
 import subprocess
 
-from .forms import Var2TexShadeForm
+from .forms import Var2TexShadeForm, GeneSymbolCheckerForm
 # Create your views here.
 
 def index(request):
@@ -39,3 +38,36 @@ def var2texshade_api(request, hgvsp):
                       'bioscripts/var2texshade.html',
                       {"error": f"Error: {E.output.decode('utf-8')}"})
     return FileResponse(open(f"{result.decode('utf-8').strip()}", 'rb'), as_attachment=True, filename=f'{hgvsp}.pdf')
+
+
+def genesymbolchecker(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = GeneSymbolCheckerForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            symbols = form.cleaned_data['symbols']
+            assembly = form.cleaned_data['assembly']
+            source = form.cleaned_data['source']
+            symbols = symbols.replace("\r\n", " ")
+            # redirect to a new URL:
+            return redirect("bioscripts:genesymbolchecker_api", source=source, assembly=assembly, symbols=symbols)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = GeneSymbolCheckerForm()
+
+    return render(request, 'bioscripts/genesymbolchecker.html', {'form': form})
+
+
+def genesymbolchecker_api(request, source, assembly, symbols):
+    module_path = settings.BASE_DIR.parent.joinpath("bioscripts/modules/genesymbolchecker/")
+    process = subprocess.Popen(f"tsp -fn {module_path.joinpath('checkgeneset.sh')} -s {source} -a {assembly} {symbols}", stdout=subprocess.PIPE, shell=True)
+
+    return StreamingHttpResponse(
+        (line.decode('utf-8') for line in process.stdout),
+        content_type="text/plain",
+        headers={'Content-Disposition': 'attachment; filename="geneset.txt"'},
+    )
