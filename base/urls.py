aa2d8db2934b83dfd87ca6c5d32bf0cb34505087
bar <bar@E15G3.home>
2023-10-22 02:55:06 +0300

    added blog post ngsoneliner

--- base/urls.py
+++ base/urls.py
@@ -6,8 +6,6 @@ from . import views
 app_name = "base"
 
 urlpatterns = [
-    # ex: /polls/
     path('', views.index, name='index'),
-    path('notes', views.notes, name='notes'),
-    # ex: /polls/5/
+    path('notes/', views.notes, name='notes'),
 ]
